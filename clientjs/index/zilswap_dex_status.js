48864997abce2f359ee66b639d8d5bb944f7cada
zilWatchDev <zilwatch.io@gmail.com>
2022-01-16 19:33:26 +0800

    Fix dXCAD price fallback to zilswap

--- clientjs/index/zilswap_dex_status.js
+++ clientjs/index/zilswap_dex_status.js
@@ -253,7 +253,7 @@ class ZilswapDexStatus {
     getZrcPriceInZilWithFallback(zrcSymbol) {
         // TODO: Remove the manual guard for dXCAD, this is to guard showing
         // zilswap price with low liquidity.
-        if (zrcSymbol !== 'dXCAD' || zrcSymbol !== 'zBRKL') {
+        if (zrcSymbol !== 'dXCAD' && zrcSymbol !== 'zBRKL') {
             let zilswapZrcPriceInZil = this.getZrcPriceInZil(zrcSymbol);
             if (zilswapZrcPriceInZil) {
                 return zilswapZrcPriceInZil;
